320d95122a89e436264eceeaaadbb96c46463234
Plenoh <tylrd529@gmail.com>
2021-09-10 05:49:33 +0900

    Improved a lot

---
+++ scripts/script.js
@@ -0,0 +1,39 @@
+function bpSmall() {
+    //console.log("bpSmall() called");
+    /*
+    jQuery("#gnb .btn-menu").on("click", function() {
+        jQuery("#gnb").toggleClass("open");
+    });
+    */
+}
+function bpMedium() {
+    //console.log("bpMedium() called");
+}
+function bpLarge() {
+    //console.log("bpLarge() called");
+}
+
+jQuery(document).ready(function() {
+    if(jQuery(window).width() <= 640) {
+        bpSmall();
+    }
+    else if(641 <= jQuery(window).width() && jQuery(window).width() <= 1024) {
+        bpMedium();
+    }
+    else {
+        bpLarge();
+    }
+});
+
+jQuery(window).resize(function() {
+    //jQuery("#gnb *").off();
+    if(jQuery(window).width() <= 640) {
+        bpSmall();
+    }
+    else if(641 <= jQuery(window).width() && jQuery(window).width() <= 1024) {
+        bpMedium();
+    }
+    else {
+        bpLarge();
+    }
+});
